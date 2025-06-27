d95620ce2f182254a345d7271d2bf92593a2e615
MuhammadShafeeque <shafeequ@uni-bremen.de>
2025-06-27 18:23:43 +0200

    working

---
+++ src/AppPlaceHolderTesting.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>German-Urdu Tutor</h1>
+        <p>Application is working!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
